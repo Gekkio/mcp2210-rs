61fc4b504e3e7099ba71631b45ca4b5dfc6acd68
Joonas Javanainen <joonas.javanainen@gmail.com>
2018-07-14 12:26:09 +0300

    New trait-based command implementation

---
+++ src/cmds.rs
@@ -0,0 +1,294 @@
+use super::{Buffer, Mcp2210Error};
+use std::cmp::min;
+use std::io::{self, Read, Write};
+use types::*;
+use utils::{as_u16, encode_utf16_to_buffer};
+
+pub trait CommandResponse {
+    fn command_response(&mut self, cmd: &Buffer, res: &mut Buffer) -> io::Result<()>;
+    fn do_command<F>(&mut self, cmd_code: u8, res: &mut Buffer, f: F) -> Result<(), Mcp2210Error>
+    where
+        F: FnOnce(&mut Buffer) -> (),
+    {
+        let mut cmd: Buffer = [0; 64];
+        cmd[0] = cmd_code;
+        f(&mut cmd);
+        self.command_response(&cmd, res).map_err(Mcp2210Error::Io)?;
+        if cmd_code != res[0] {
+            return Err(Mcp2210Error::CommandCode {
+                expected: cmd_code,
+                actual: res[0],
+            });
+        }
+        match res[1] {
+            0x00 => Ok(()),
+            0xf7 => Err(Mcp2210Error::Unavailable),
+            0xf8 => Err(Mcp2210Error::Busy),
+            0xf9 => Err(Mcp2210Error::UnknownCommandCode(cmd_code)),
+            0xfa => Err(Mcp2210Error::EepromWrite),
+            0xfb => Err(Mcp2210Error::AccessDenied),
+            0xfc => Err(Mcp2210Error::AccessRejected),
+            0xfd => Err(Mcp2210Error::AccessDeniedRetry),
+            err_code => Err(Mcp2210Error::UnknownErrorCode(err_code)),
+        }
+    }
+    fn do_sub_command<F>(
+        &mut self,
+        cmd_code: u8,
+        sub_cmd_code: u8,
+        res: &mut Buffer,
+        f: F,
+    ) -> Result<(), Mcp2210Error>
+    where
+        F: FnOnce(&mut Buffer) -> (),
+    {
+        self.do_command(cmd_code, res, |cmd| {
+            cmd[1] = sub_cmd_code;
+            f(cmd);
+        })?;
+        if res[2] != sub_cmd_code {
+            Err(Mcp2210Error::SubCommandCode {
+                expected: sub_cmd_code,
+                actual: res[2],
+            })
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl<T> CommandResponse for T
+where
+    T: Read + Write + Sized,
+{
+    fn command_response(&mut self, cmd: &Buffer, res: &mut Buffer) -> io::Result<()> {
+        self.write_all(cmd)?;
+        self.read_exact(res)
+    }
+}
+
+pub trait Commands: CommandResponse {
+    fn get_chip_status(&mut self) -> Result<ChipStatus, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x10, &mut res, |_| {})?;
+        ChipStatus::from_buffer(&res).map_err(Mcp2210Error::InvalidResponse)
+    }
+    fn cancel_spi_transfer(&mut self) -> Result<ChipStatus, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x11, &mut res, |_| {})?;
+        ChipStatus::from_buffer(&res).map_err(Mcp2210Error::InvalidResponse)
+    }
+    fn get_interrupt_event_counter(&mut self) -> Result<u16, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x12, &mut res, |cmd| {
+            cmd[1] = 0xff;
+        })?;
+        Ok(as_u16(res[4], res[5]))
+    }
+    fn reset_interrupt_event_counter(&mut self) -> Result<u16, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x12, &mut res, |cmd| {
+            cmd[1] = 0x00;
+        })?;
+        Ok(as_u16(res[4], res[5]))
+    }
+    fn get_chip_settings(&mut self) -> Result<ChipSettings, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x20, &mut res, |_| {})?;
+        ChipSettings::from_buffer(&res).map_err(Mcp2210Error::InvalidResponse)
+    }
+    fn set_chip_settings(&mut self, settings: &ChipSettings) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x21, &mut res, |cmd| {
+            settings.write_to_buffer(cmd);
+        })
+    }
+    fn set_gpio_value(&mut self, value: GpioValue) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x30, &mut res, |cmd| {
+            let value = value.bits();
+            cmd[4] = value as u8;
+            cmd[5] = (value >> 8) as u8;
+        })
+    }
+    fn get_gpio_value(&mut self) -> Result<GpioValue, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x31, &mut res, |_| {})?;
+        Ok(GpioValue::from_bits_truncate(as_u16(res[4], res[5])))
+    }
+    fn set_gpio_direction(&mut self, direction: GpioDirection) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x32, &mut res, |cmd| {
+            let direction = direction.bits();
+            cmd[4] = direction as u8;
+            cmd[5] = (direction >> 8) as u8;
+        })
+    }
+    fn get_gpio_direction(&mut self) -> Result<GpioDirection, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x33, &mut res, |_| {})?;
+        Ok(GpioDirection::from_bits_truncate(as_u16(res[4], res[5])))
+    }
+    fn set_spi_transfer_settings(
+        &mut self,
+        settings: &SpiTransferSettings,
+    ) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x40, &mut res, |cmd| {
+            settings.write_to_buffer(cmd);
+        })
+    }
+    fn get_spi_transfer_settings(&mut self) -> Result<SpiTransferSettings, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x41, &mut res, |_| {})?;
+        SpiTransferSettings::from_buffer(&res).map_err(Mcp2210Error::InvalidResponse)
+    }
+    fn spi_transfer<'a>(
+        &mut self,
+        data: &[u8],
+        res: &'a mut Buffer,
+    ) -> Result<SpiTransferResponse<'a>, Mcp2210Error> {
+        if data.len() > 60 {
+            return Err(Mcp2210Error::PayloadSize(data.len()));
+        }
+        let mosi_len = min(data.len(), 60);
+        self.do_command(0x42, res, |cmd| {
+            cmd[1] = mosi_len as u8;
+            cmd[4..][..mosi_len].copy_from_slice(&data[..mosi_len]);
+        })?;
+        let miso_len = res[2] as usize;
+        Ok(SpiTransferResponse {
+            data: &res[4..][..miso_len],
+            status: SpiTransferStatus::from_u8(res[3]).map_err(|v| {
+                Mcp2210Error::InvalidResponse(format!("Invalid SPI transfer status: {:02x}", v))
+            })?,
+        })
+    }
+    fn read_eeprom(&mut self, addr: u8) -> Result<u8, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x50, &mut res, |cmd| {
+            cmd[1] = addr;
+        })?;
+        if res[2] != addr {
+            return Err(Mcp2210Error::InvalidResponse(format!(
+                "Invalid EEPROM address {:2x}",
+                addr
+            )));
+        }
+        Ok(res[3])
+    }
+    fn write_eeprom(&mut self, addr: u8, data: u8) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x51, &mut res, |cmd| {
+            cmd[1] = addr;
+            cmd[2] = data;
+        })
+    }
+    fn set_nvram_spi_transfer_settings(
+        &mut self,
+        settings: &SpiTransferSettings,
+    ) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_sub_command(0x60, 0x10, &mut res, |cmd| {
+            settings.write_to_buffer(cmd);
+        })
+    }
+    fn set_nvram_chip_settings(
+        &mut self,
+        settings: &ChipSettings,
+        password: Option<&[u8; 8]>,
+    ) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_sub_command(0x60, 0x20, &mut res, |cmd| {
+            settings.write_to_buffer(cmd);
+            if let Some(password) = password {
+                cmd[19..27].copy_from_slice(password);
+            }
+        })
+    }
+    fn set_nvram_usb_parameters(&mut self, params: &UsbParameters) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_sub_command(0x60, 0x30, &mut res, |cmd| {
+            params.write_to_buffer(cmd);
+        })
+    }
+    fn set_nvram_usb_product_name(&mut self, name: &str) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        let size = name.encode_utf16().count();
+        if size > 29 {
+            return Err(Mcp2210Error::StringSize(size));
+        }
+        self.do_sub_command(0x60, 0x40, &mut res, |cmd| {
+            cmd[4] = (size as u8) * 2 + 2;
+            cmd[5] = 0x03;
+            encode_utf16_to_buffer(name, &mut cmd[6..]);
+        })
+    }
+    fn set_nvram_usb_vendor_name(&mut self, name: &str) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        let size = name.encode_utf16().count();
+        if size > 29 {
+            return Err(Mcp2210Error::StringSize(size));
+        }
+        self.do_sub_command(0x60, 0x50, &mut res, |cmd| {
+            cmd[4] = (size as u8) * 2 + 2;
+            cmd[5] = 0x03;
+            encode_utf16_to_buffer(name, &mut cmd[6..]);
+        })
+    }
+    fn get_nvram_spi_transfer_settings(&mut self) -> Result<SpiTransferSettings, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_sub_command(0x61, 0x10, &mut res, |_| {})?;
+        SpiTransferSettings::from_buffer(&res).map_err(Mcp2210Error::InvalidResponse)
+    }
+    fn get_nvram_chip_settings(&mut self) -> Result<ChipSettings, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_sub_command(0x61, 0x20, &mut res, |_| {})?;
+        ChipSettings::from_buffer(&res).map_err(Mcp2210Error::InvalidResponse)
+    }
+    fn get_nvram_usb_parameters(&mut self) -> Result<UsbParameters, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_sub_command(0x61, 0x30, &mut res, |_| {})?;
+        UsbParameters::from_buffer(&res).map_err(Mcp2210Error::InvalidResponse)
+    }
+    fn get_nvram_usb_product_name(&mut self) -> Result<String, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_sub_command(0x61, 0x40, &mut res, |_| {})?;
+        let str_bytes = (res[4] - 2) as usize;
+        let str_chars = str_bytes / 2;
+        let mut char_buf = [0; 29];
+        for (idx, chunk) in res[6..][..str_bytes].chunks(2).enumerate() {
+            char_buf[idx] = as_u16(chunk[0], chunk[1]);
+        }
+        Ok(String::from_utf16_lossy(&char_buf[..str_chars]))
+    }
+    fn get_nvram_usb_vendor_name(&mut self) -> Result<String, Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_sub_command(0x61, 0x50, &mut res, |_| {})?;
+        let str_bytes = (res[4] - 2) as usize;
+        let str_chars = str_bytes / 2;
+        let mut char_buf = [0; 29];
+        for (idx, chunk) in res[6..][..str_bytes].chunks(2).enumerate() {
+            char_buf[idx] = as_u16(chunk[0], chunk[1]);
+        }
+        Ok(String::from_utf16_lossy(&char_buf[..str_chars]))
+    }
+    fn send_access_password(&mut self, password: &[u8; 8]) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x70, &mut res, |cmd| {
+            cmd[4..11].copy_from_slice(password);
+        })
+    }
+    fn request_bus_release(&mut self, ack_value: bool) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
+        self.do_command(0x80, &mut res, |cmd| {
+            cmd[1] = if ack_value { 0x01 } else { 0x00 };
+        })
+    }
+}
+
+impl<T> Commands for T
+where
+    T: CommandResponse,
+{
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,19 @@ extern crate failure;
 extern crate libudev;
 extern crate nix;
 
+mod cmds;
 mod types;
 mod utils;
 
+pub use cmds::*;
 pub use types::*;
 
 use libudev::Device;
 use std::cmp::min;
 use std::ffi::OsStr;
 use std::fs::{File, OpenOptions};
-use std::io::{self, Read, Write};
+use std::io;
 use std::path::{Path, PathBuf};
-
-use utils::*;
 
 #[derive(Fail, Debug)]
 pub enum Mcp2210Error {
@@ -61,7 +61,12 @@ pub type Buffer = [u8; 64];
 
 pub struct Mcp2210 {
     file: File,
-    response_buffer: Buffer,
+}
+
+impl CommandResponse for Mcp2210 {
+    fn command_response(&mut self, cmd: &Buffer, res: &mut Buffer) -> io::Result<()> {
+        self.file.command_response(cmd, res)
+    }
 }
 
 impl Mcp2210 {
@@ -71,265 +76,17 @@ impl Mcp2210 {
             .write(true)
             .open(path)
             .map_err(Mcp2210Error::Io)?;
-        Ok(Mcp2210 {
-            file,
-            response_buffer: [0; 64],
-        })
-    }
-    fn do_command<F>(&mut self, cmd_code: u8, f: F) -> Result<(), Mcp2210Error>
-    where
-        F: FnOnce(&mut Buffer) -> (),
-    {
-        let mut cmd: Buffer = [0; 64];
-        cmd[0] = cmd_code;
-        f(&mut cmd);
-        self.file.write_all(&cmd).map_err(Mcp2210Error::Io)?;
-        self.file
-            .read_exact(&mut self.response_buffer)
-            .map_err(Mcp2210Error::Io)?;
-        if cmd_code != self.response_buffer[0] {
-            return Err(Mcp2210Error::CommandCode {
-                expected: cmd_code,
-                actual: self.response_buffer[0],
-            });
-        }
-        match self.response_buffer[1] {
-            0x00 => Ok(()),
-            0xf7 => Err(Mcp2210Error::Unavailable),
-            0xf8 => Err(Mcp2210Error::Busy),
-            0xf9 => Err(Mcp2210Error::UnknownCommandCode(cmd_code)),
-            0xfa => Err(Mcp2210Error::EepromWrite),
-            0xfb => Err(Mcp2210Error::AccessDenied),
-            0xfc => Err(Mcp2210Error::AccessRejected),
-            0xfd => Err(Mcp2210Error::AccessDeniedRetry),
-            err_code => Err(Mcp2210Error::UnknownErrorCode(err_code)),
-        }
-    }
-    fn do_sub_command<F>(
-        &mut self,
-        cmd_code: u8,
-        sub_cmd_code: u8,
-        f: F,
-    ) -> Result<(), Mcp2210Error>
-    where
-        F: FnOnce(&mut Buffer) -> (),
-    {
-        self.do_command(cmd_code, |cmd| {
-            cmd[1] = sub_cmd_code;
-            f(cmd);
-        })?;
-        if self.response_buffer[2] != sub_cmd_code {
-            Err(Mcp2210Error::SubCommandCode {
-                expected: sub_cmd_code,
-                actual: self.response_buffer[2],
-            })
-        } else {
-            Ok(())
-        }
-    }
-    pub fn get_chip_status(&mut self) -> Result<ChipStatus, Mcp2210Error> {
-        self.do_command(0x10, |_| {})?;
-        ChipStatus::from_buffer(&self.response_buffer).map_err(Mcp2210Error::InvalidResponse)
-    }
-    pub fn cancel_spi_transfer(&mut self) -> Result<ChipStatus, Mcp2210Error> {
-        self.do_command(0x11, |_| {})?;
-        ChipStatus::from_buffer(&self.response_buffer).map_err(Mcp2210Error::InvalidResponse)
-    }
-    pub fn get_interrupt_event_counter(&mut self) -> Result<u16, Mcp2210Error> {
-        self.do_command(0x12, |cmd| {
-            cmd[1] = 0xff;
-        })?;
-        Ok(as_u16(self.response_buffer[4], self.response_buffer[5]))
-    }
-    pub fn reset_interrupt_event_counter(&mut self) -> Result<u16, Mcp2210Error> {
-        self.do_command(0x12, |cmd| {
-            cmd[1] = 0x00;
-        })?;
-        Ok(as_u16(self.response_buffer[4], self.response_buffer[5]))
-    }
-    pub fn get_chip_settings(&mut self) -> Result<ChipSettings, Mcp2210Error> {
-        self.do_command(0x20, |_| {})?;
-        ChipSettings::from_buffer(&self.response_buffer).map_err(Mcp2210Error::InvalidResponse)
-    }
-    pub fn set_chip_settings(&mut self, settings: &ChipSettings) -> Result<(), Mcp2210Error> {
-        self.do_command(0x21, |cmd| {
-            settings.write_to_buffer(cmd);
-        })
-    }
-    pub fn set_gpio_value(&mut self, value: GpioValue) -> Result<(), Mcp2210Error> {
-        self.do_command(0x30, |cmd| {
-            let value = value.bits();
-            cmd[4] = value as u8;
-            cmd[5] = (value >> 8) as u8;
-        })
-    }
-    pub fn get_gpio_value(&mut self) -> Result<GpioValue, Mcp2210Error> {
-        self.do_command(0x31, |_| {})?;
-        Ok(GpioValue::from_bits_truncate(as_u16(
-            self.response_buffer[4],
-            self.response_buffer[5],
-        )))
-    }
-    pub fn set_gpio_direction(&mut self, direction: GpioDirection) -> Result<(), Mcp2210Error> {
-        self.do_command(0x32, |cmd| {
-            let direction = direction.bits();
-            cmd[4] = direction as u8;
-            cmd[5] = (direction >> 8) as u8;
-        })
-    }
-    pub fn get_gpio_direction(&mut self) -> Result<GpioDirection, Mcp2210Error> {
-        self.do_command(0x33, |_| {})?;
-        Ok(GpioDirection::from_bits_truncate(as_u16(
-            self.response_buffer[4],
-            self.response_buffer[5],
-        )))
-    }
-    pub fn set_spi_transfer_settings(
-        &mut self,
-        settings: &SpiTransferSettings,
-    ) -> Result<(), Mcp2210Error> {
-        self.do_command(0x40, |cmd| {
-            settings.write_to_buffer(cmd);
-        })
-    }
-    pub fn get_spi_transfer_settings(&mut self) -> Result<SpiTransferSettings, Mcp2210Error> {
-        self.do_command(0x41, |_| {})?;
-        SpiTransferSettings::from_buffer(&self.response_buffer)
-            .map_err(Mcp2210Error::InvalidResponse)
-    }
-    pub fn spi_transfer(&mut self, data: &[u8]) -> Result<SpiTransferResponse, Mcp2210Error> {
-        if data.len() > 60 {
-            return Err(Mcp2210Error::PayloadSize(data.len()));
-        }
-        let mosi_len = min(data.len(), 60);
-        self.do_command(0x42, |cmd| {
-            cmd[1] = mosi_len as u8;
-            cmd[4..][..mosi_len].copy_from_slice(&data[..mosi_len]);
-        })?;
-        let miso_len = self.response_buffer[2] as usize;
-        Ok(SpiTransferResponse {
-            data: &self.response_buffer[4..][..miso_len],
-            status: SpiTransferStatus::from_u8(self.response_buffer[3]).map_err(|v| {
-                Mcp2210Error::InvalidResponse(format!("Invalid SPI transfer status: {:02x}", v))
-            })?,
-        })
-    }
-    pub fn read_eeprom(&mut self, addr: u8) -> Result<u8, Mcp2210Error> {
-        self.do_command(0x50, |cmd| {
-            cmd[1] = addr;
-        })?;
-        if self.response_buffer[2] != addr {
-            return Err(Mcp2210Error::InvalidResponse(format!(
-                "Invalid EEPROM address {:2x}",
-                addr
-            )));
-        }
-        Ok(self.response_buffer[3])
-    }
-    pub fn write_eeprom(&mut self, addr: u8, data: u8) -> Result<(), Mcp2210Error> {
-        self.do_command(0x51, |cmd| {
-            cmd[1] = addr;
-            cmd[2] = data;
-        })
-    }
-    pub fn set_nvram_spi_transfer_settings(
-        &mut self,
-        settings: &SpiTransferSettings,
-    ) -> Result<(), Mcp2210Error> {
-        self.do_sub_command(0x60, 0x10, |cmd| {
-            settings.write_to_buffer(cmd);
-        })
-    }
-    pub fn set_nvram_chip_settings(
-        &mut self,
-        settings: &ChipSettings,
-        password: Option<&[u8; 8]>,
-    ) -> Result<(), Mcp2210Error> {
-        self.do_sub_command(0x60, 0x20, |cmd| {
-            settings.write_to_buffer(cmd);
-            if let Some(password) = password {
-                cmd[19..27].copy_from_slice(password);
-            }
-        })
-    }
-    pub fn set_nvram_usb_parameters(&mut self, params: &UsbParameters) -> Result<(), Mcp2210Error> {
-        self.do_sub_command(0x60, 0x30, |cmd| {
-            params.write_to_buffer(cmd);
-        })
-    }
-    pub fn set_nvram_usb_product_name(&mut self, name: &str) -> Result<(), Mcp2210Error> {
-        let size = name.encode_utf16().count();
-        if size > 29 {
-            return Err(Mcp2210Error::StringSize(size));
-        }
-        self.do_sub_command(0x60, 0x40, |cmd| {
-            cmd[4] = (size as u8) * 2 + 2;
-            cmd[5] = 0x03;
-            encode_utf16_to_buffer(name, &mut cmd[6..]);
-        })
-    }
-    pub fn set_nvram_usb_vendor_name(&mut self, name: &str) -> Result<(), Mcp2210Error> {
-        let size = name.encode_utf16().count();
-        if size > 29 {
-            return Err(Mcp2210Error::StringSize(size));
-        }
-        self.do_sub_command(0x60, 0x50, |cmd| {
-            cmd[4] = (size as u8) * 2 + 2;
-            cmd[5] = 0x03;
-            encode_utf16_to_buffer(name, &mut cmd[6..]);
-        })
-    }
-    pub fn get_nvram_spi_transfer_settings(&mut self) -> Result<SpiTransferSettings, Mcp2210Error> {
-        self.do_sub_command(0x61, 0x10, |_| {})?;
-        SpiTransferSettings::from_buffer(&self.response_buffer)
-            .map_err(Mcp2210Error::InvalidResponse)
-    }
-    pub fn get_nvram_chip_settings(&mut self) -> Result<ChipSettings, Mcp2210Error> {
-        self.do_sub_command(0x61, 0x20, |_| {})?;
-        ChipSettings::from_buffer(&self.response_buffer).map_err(Mcp2210Error::InvalidResponse)
-    }
-    pub fn get_nvram_usb_parameters(&mut self) -> Result<UsbParameters, Mcp2210Error> {
-        self.do_sub_command(0x61, 0x30, |_| {})?;
-        UsbParameters::from_buffer(&self.response_buffer).map_err(Mcp2210Error::InvalidResponse)
-    }
-    pub fn get_nvram_usb_product_name(&mut self) -> Result<String, Mcp2210Error> {
-        self.do_sub_command(0x61, 0x40, |_| {})?;
-        let str_bytes = (self.response_buffer[4] - 2) as usize;
-        let str_chars = str_bytes / 2;
-        let mut char_buf = [0; 29];
-        for (idx, chunk) in self.response_buffer[6..][..str_bytes].chunks(2).enumerate() {
-            char_buf[idx] = as_u16(chunk[0], chunk[1]);
-        }
-        Ok(String::from_utf16_lossy(&char_buf[..str_chars]))
-    }
-    pub fn get_nvram_usb_vendor_name(&mut self) -> Result<String, Mcp2210Error> {
-        self.do_sub_command(0x61, 0x50, |_| {})?;
-        let str_bytes = (self.response_buffer[4] - 2) as usize;
-        let str_chars = str_bytes / 2;
-        let mut char_buf = [0; 29];
-        for (idx, chunk) in self.response_buffer[6..][..str_bytes].chunks(2).enumerate() {
-            char_buf[idx] = as_u16(chunk[0], chunk[1]);
-        }
-        Ok(String::from_utf16_lossy(&char_buf[..str_chars]))
-    }
-    pub fn send_access_password(&mut self, password: &[u8; 8]) -> Result<(), Mcp2210Error> {
-        self.do_command(0x70, |cmd| {
-            cmd[4..11].copy_from_slice(password);
-        })
-    }
-    pub fn request_bus_release(&mut self, ack_value: bool) -> Result<(), Mcp2210Error> {
-        self.do_command(0x80, |cmd| {
-            cmd[1] = if ack_value { 0x01 } else { 0x00 };
-        })
+        Ok(Mcp2210 { file })
     }
     pub fn spi_transfer_to_end(
         &mut self,
         mut data: &[u8],
         buf: &mut Vec<u8>,
     ) -> Result<(), Mcp2210Error> {
+        let mut res: Buffer = [0; 64];
         {
             let len = min(data.len(), 60);
-            let res = self.spi_transfer(&data[..len])?;
+            let res = self.spi_transfer(&data[..len], &mut res)?;
             data = &data[len..];
             if res.status != SpiTransferStatus::Started {
                 return Err(Mcp2210Error::TransferStatus(res.status));
@@ -337,7 +94,7 @@ impl Mcp2210 {
         }
         loop {
             let len = min(data.len(), 60);
-            match self.spi_transfer(&data[..len]) {
+            match self.spi_transfer(&data[..len], &mut res) {
                 Ok(res) => {
                     data = &data[len..];
                     buf.extend(res.data);
