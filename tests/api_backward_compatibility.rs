0a92feba3927c3b87666b864336ee43448fb9050
Jonathan Lindsey <jonathan@matechservices.com>
2023-11-13 14:54:22 -0600

    Document some times when backward compatibility breaks.

--- tests/api_backward_compatibility.rs
+++ tests/api_backward_compatibility.rs
@@ -4,18 +4,21 @@
 #[ignore = "Only testing that this builds. If a device is connected, we don't know if it's safe to use it."]
 fn pre_windows_support_examples() {
     use mcp2210::{Commands, Mcp2210, SpiMode, SpiTransferSettings};
-    use std::path::PathBuf;
 
     // The scan_devices_with_filter() function used the libudev::Device type. Consumers of this function
-    // will likely need to rewrite the closure
+    // will likely need to rewrite the closure.
 
     {
+        #[allow(deprecated)]
         let _ = Mcp2210::open("/dev/hidraw0").expect("Failed to open device");
     }
 
     {
-        let devices: Vec<PathBuf> = mcp2210::scan_devices().expect("Failed to scan devices");
-        let device: &PathBuf = devices.iter().next().expect("No devices found");
+        // If consumers of this crate depend on either the scan_devices() or scan_devices_with_filter()
+        // function returning a Vec<PathBuf>, they will break.
+        let devices = mcp2210::scan_devices().expect("Failed to scan devices");
+        let device = devices.iter().next().expect("No devices found");
+        #[allow(deprecated)]
         let mut mcp: Mcp2210 = Mcp2210::open(&device).expect("Failed to open device");
         mcp.set_spi_transfer_settings(&SpiTransferSettings {
             bit_rate: 1_000,
